c024cf0d5b1e787aeff0d6cc57a5dc9bf162ce82
Vitor <costasousavitor@outlook.com>
2024-04-25 09:23:31 -0300

    first commit

---
+++ src/assets/components/Card/index.jsx
@@ -0,0 +1,9 @@
+import { Card } from "./style";
+
+export const CardComponent = () => {
+  return (
+    <Card>
+      <h1>Vitor Sousa</h1>
+    </Card>
+  );
+};
---
+++ src/assets/pages/App.jsx
@@ -0,0 +1,11 @@
+import { CardComponent } from "../components/Card"
+
+
+
+function App() {
+  return(
+    <CardComponent></CardComponent>
+  )
+}
+
+export default App
